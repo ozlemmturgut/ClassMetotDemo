b60093fb50dfea1f2df198fe4bc2cf249d42ab7c
ozlemmturgut <55889487+ozlemmturgut@users.noreply.github.com>
2021-02-09 12:14:36 +0300

    9.02.2021

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Methods
 {
@@ -11,13 +12,23 @@ namespace Methods
             //Add();
             //Add();
             //int result1 = Add2();
-            int number1 = 20;
-            int number2 = 100;
-            var result2 = Add3(number1, number2);
-            Console.WriteLine(result2);
-            Console.WriteLine(number1); // number1 = 20
-            Console.ReadLine();
+            //int number1 = 20;
+            //int number2 = 100;
+            ////var result2 = Add3(ref number1, number2);
+            //var result2 = Add3(out number1, number2);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(number1); // number1 = 20 --> ref olunca number1 = 30
             
+            // ref -> number1 !ın ilk değerinin plması lazım
+            // out'ta böyle bir zorunluluk yok
+
+
+            Console.WriteLine(Multiply(2,4));
+            Console.WriteLine(Multiply(2,4,5));
+
+            Console.WriteLine(Add4(1,2,3,4,5,6));
+
+            Console.ReadLine();
         }
 
         static void Add()
@@ -33,10 +44,36 @@ namespace Methods
             return result;
         }
 
-        static int Add3(int number1, int number2)
+        /*static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }*/
+
+        static int Add3(out int number1, int number2)
         {
             number1 = 30;
             return number1 + number2;
         }
+        // Farklı parametreler ama aynı metot ismi --> Method Overloading
+        static int Multiply(int number1, int number2) //Metodun imzası
+        {
+            return number1 * number2;
+         }
+
+        static int Multiply(int number1, int number2, int number3) 
+        {
+            return number1 * number2 * number3;
+        }
+        // aynı tipte istediğimiz kadar parametre gönderebiliriz --> params
+        //params son parametre olmak zorunda
+        static int Add4(int number, params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
+
+
+
     }
 }
